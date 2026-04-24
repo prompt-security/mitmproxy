895d98abdf58bb2e4ab8b2d0ba5f4c2c864496fa
Vitaly Neyman <vitaly@prompt.security>
2026-04-24 16:02:03 +0200

    PROE-5647: Speed up ps-agent builds - bump version to 12.2.1+ps1

--- mitmproxy/version.py
+++ mitmproxy/version.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import sys
 
-VERSION = "12.2.1"
+VERSION = "12.2.1+ps1"
 MITMPROXY = "mitmproxy " + VERSION
 
 # Serialization format version. This is displayed nowhere, it just needs to be incremented by one
